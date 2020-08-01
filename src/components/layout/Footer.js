534a34d57155c74c795074ad41a029095a7c44d6
saxzzy94 <kelvinagbonze@gmail.com>
2020-08-01 12:58:56 +0100

    adjusted footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,29 +6,30 @@ const Footer = () => {
 			className='ui vertical footer segment inverted '
 			style={{ height: "300px" }}
 		>
-			<div className='ui container segment inverted'>
+			<div className='ui menu  inverted segment'>
 				<div
-					class='ui four column grid double stackable segment divided inverted'
-					style={{ height: "200px" }}
+					class='ui center aligned  column grid equal height stackable segment divided inverted'
+					style={{ height: "100%", width: "100%" }}
 				>
-					<div className='column'>
-						<h2 className='ui header inverted'>About Us</h2>
-						<div className='item'>Our mission</div>
-						<div className='item'>we Our Vision</div>
-						<div className='item'>Our Goal</div>
+					<div className=' four wide column segment inverted'>
+						<h2 className='ui header inverted segment'>About Us</h2>
+						<div className='ui list link inverted segment'>
+							<div className='item'>Our mission</div>
+							<div className='item'>we Our Vision</div>
+							<div className='item'>Our Goal</div>
+						</div>
 					</div>
-					<div class='column'>
-						<h2 className='ui header inverted'>Our Services</h2>
-						<div className=' menu'>
+					<div class='four wide column inverted segment'>
+						<h2 className='ui header inverted segment'>Our Services</h2>
+						<div className=' ui list link inverted segment'>
 							<div className='item'>Courses</div>
 							<div className='item'>Past Questions</div>
 							<div className='item'></div>
 						</div>
 					</div>
-
-					<div class='column'>
-						<h2 className='ui header inverted'>Follow Us</h2>
-						<div className='menu'>
+					<div class='four wide column segment inverted '>
+						<h2 className='ui header segment inverted'>Follow Us</h2>
+						<div className='ui list link inverted segment'>
 							<div className='item'>
 								<i className='green facebook f icon'></i>Facebook
 							</div>
@@ -40,9 +41,9 @@ const Footer = () => {
 							</div>
 						</div>
 					</div>
-					<div class='column'>
+					<div class='four wide column segment inverted'>
 						<h2 className='ui header inverted'>Contact us</h2>
-						<div className='menu'>
+						<div className='ui list link inverted segment'>
 							<div className='item'>
 								<i className='green home icon'></i>6 Gbemisola Street, opposite
 								Kalakuta Museum, First Bank Bus stop, Allen Avenue, Ikeja Lagos
